615a9b45945cc62882e9c4119114f3351715ef1c
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-22 00:37:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import torch
 
 # Model parameters
 IN_CHANNELS = 3
-OUT_CHANNELS = 1
+OUT_CHANNELS = 3
 NUM_LAYERS = 6
 BASE_CHANNELS = 128
 KERNEL_SIZE = 3
@@ -13,7 +13,7 @@ DROPOUT_RATE = 0.3
 UPSAMPLING_METHOD = "TransposedConv"
 
 # Training parameters
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 LEARNING_RATE = 0.0005
 NUM_EPOCHS = 300
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -22,3 +22,6 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 IMG_SIZE = (512, 512)
 AUGMENTATIONS = True
 VAL_SPLIT = 0.15
+
+# Additional parameters
+EXPERIMENT_NAME = "experiment_0"
