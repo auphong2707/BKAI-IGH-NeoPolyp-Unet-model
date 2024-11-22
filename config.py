805464d57576d1e0e877149bd6cbbf00020b939e
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-22 15:36:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DROPOUT_RATE = 0.3
 UPSAMPLING_METHOD = "TransposedConv"
 
 # Training parameters
-BATCH_SIZE = 16
+BATCH_SIZE = 8
 LEARNING_RATE = 0.0001
 NUM_EPOCHS = 300
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
